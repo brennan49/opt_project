b0f8d2515dd655a2ac604b0f71c4bbab55ee3e02
brennan49 <brennan_49@live.com>
2016-06-21 17:28:19 -0500

    second submit, basic sendEmail script implemented

--- automated_delete.py
+++ automated_delete.py
@@ -1,5 +1,6 @@
 import urllib
 import requests
+import smtplib
 
 user = '' #To log onto SwiftKanban, for use with API calls
 passwrd = ''
@@ -8,15 +9,12 @@ url = 'https://login.swiftkanban.com/axis2/services/KanbanCardService?wsdl'
 
 #likely not necessary
 def _getUrl(path):
- return 'https://SwiftKanban.org' + path
+     return 'https://SwiftKanban.org' + path
 
 #request access to SwiftKanban
 def requestAccess(url):
- resp = requests.get('https://login.swiftkanban.com/axis2/services/KanbanCardService?wsdl', auth=('user', 'pass'))
- if(resp != 200):
-  return "Error get request failed."
- else:
-  print("request passed!")
+    kanbanApi = requests.urlopen('https://login.swiftkanban.com/axis2/services/KanbanCardService?wsdl', auth=('user', 'pass'))
+
 
 """call necessary API (getAllActiveUsersInOrg?) to get user data followed by getting
     the users information (getUserData?).  will place this information in a list."""
@@ -27,5 +25,5 @@ def getUserInfo(userName):
     still not sure if I want to implement creating two bins
     for people that respond yes and those that respond no to the
     e-mail or do that in another function."""
-def automatedEmail(userName):
+def automatedEmail(userName, users):
  {}
---
+++ emailScript.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def sendMail(sender, userPass, toaddr):
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = toaddr
+    msg['Subject'] = 'Automated message'
+
+    body = "This is an automated test message to multiple parties"
+    msg.attach(MIMEText(body, 'plain'))
+    #what are these values below for optum's servers?
+    server = smtplib.SMTP('casx10.uhc.com', 587)
+    server.starttls()
+    server.login(sender, userPass)
+    text = msg.as_string()
+    server.sendmail(sender, toaddr, text)
+    server.quit()
+
+sendees = ["devlin.brennan@optum.com", "tien.bui@optum.com"]
+for sendee in sendees:
+    sendMail("devlin.brennan@optum.com", "devBrenn49", sendee)
+
